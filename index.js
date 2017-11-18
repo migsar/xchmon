d502cf48311843e11f7bb0a36a4fab90dce1c5dc
Migsar Navarro <migsar.navarro@gmail.com>
2017-11-18 14:07:20 -0600

    Logger added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
+const { join } = require('path');
 const App = require('./src/App');
 const bitso = require('./src/xchs/bitso.json');
 
+const logDir = process.env['LOG_DIR'] || 'logs';
+const logFile = process.env['LOG_FILE'] || 'app.log';
 const app = new App();
 
 app.configureLogger({
-  level: 'debug',
-  file: 'logs/app.log'
+  level: process.env['LOG_LEVEL'] || 'prod',
+  file: join(__dirname, logDir, logFile)
 });
 app.addConsumer(bitso);
 app.start();
--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
-class App{
+const winston = require('winston');
+const Context = require('./Context');
 
+const state = Symbol('state');
+
+class App{
+  constructor(){
+    this[state] = {};
+    this.configureContext();
+  }
+
+  configureLogger(options) {
+    // todo: logs directory must exist or log is not created and no message is displayed
+    this[state].logger = winston.createLogger({
+      level: options.level,
+      transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+          filename: options.file
+        })
+      ]
+    });
+  }
+
+  configureContext() {
+    this[state].context = new Context;
+  }
+
+  addConsumer() {
+
+  }
+
+  start() {
+    this[state].logger.info('Xchmon started...');
+  }
 }
 
 module.exports = App;
