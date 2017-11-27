d52f08abf67cc4bcee6df5ec56dfe669aba1ddb1
Eugenio Marcelo Melendez <eugenio.mg@gmail.com>
2017-11-26 21:54:49 -0600

    Trade payload for bitso and print to console

--- app.js
+++ app.js
@@ -1,33 +1,64 @@
 var W3CWebSocket = require('websocket').w3cwebsocket;
 
-var client = new W3CWebSocket('ws://ws.bitso.com/', 'echo-protocol');
+var client = new W3CWebSocket('wss://ws.bitso.com', []);
 
-client.onerror = function() {
+client.onerror = function () {
     console.log('Connection Error');
 };
 
-client.onopen = function() {
+client.onopen = function () {
     console.log('WebSocket Client Connected');
-
     startApp();
 };
 
-client.onclose = function() {
+client.onclose = function () {
     console.log('echo-protocol Client Closed');
 };
 
-client.onmessage = function(e) {
-    if (typeof e.data === 'string') {
-        console.log("Received: '" + e.data + "'");
+client.onmessage = function (message) {
+    var data = JSON.parse(message.data);
+    /**
+     * Trade Payload
+     * i = Integer: counter
+     * a = String: # BTC
+     * r = String: precio en MXN
+     * v = String: a * r
+     * t = Integer: (1 = tradeout | 0 = tradein)
+     * mo = String: ?
+     * to = String: ?
+     */
+    if (data.type == 'trades' && data.payload) {
+
+        console.log("Received a tarde!");
+        for (var i in data.payload) {
+            var trade = data.payload[i];
+
+            var output = '[' + trade['i'] + '] ' + trade['a'] + ' BTC @ ' + trade['r'] + ' MXN = ' + trade['v'] + ' MXN';
+            console.log(output);
+        }
     }
+    //else if (data.type == 'diff-orders' && data.payload) {
+    //
+    //}
+    //else if (data.type == 'orders' && data.payload) {
+    //    
+    //}
+    //data.type == 'diff-orders'
+    //data.type == 'ka'
 };
 
-startApp = function() {
-  function subscribe() {
-      if (client.readyState === client.OPEN) {
-        client.send(JSON.stringify({ action: 'subscribe', book: 'btc_mxn', type: 'trades' }));
-      }
-  }
+startApp = function () {
+    function subscribe() {
+        if (client.readyState === client.OPEN) {
+            client.send(JSON.stringify({
+                action: 'subscribe',
+                book: 'btc_mxn',
+                type: 'trades'
+            }));
+            //client.send(JSON.stringify({ action: 'subscribe', book: 'btc_mxn', type: 'diff-orders' }));
+            //client.send(JSON.stringify({ action: 'subscribe', book: 'btc_mxn', type: 'orders' }));
+        }
+    }
 
-  subscribe();
+    subscribe();
 }
